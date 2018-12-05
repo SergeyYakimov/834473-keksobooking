882244188d28dd8d4bf4481dc721e755678f5464
SergeyYakimov <teriertis@yandex.ru>
2018-12-05 19:19:32 +0800

    Made module - card.js

--- js/map.js
+++ js/map.js
@@ -21,7 +21,6 @@ var filtersContainer = map.querySelector('.map__filters-container');
 var pinTemplate = document.querySelector('#pin').content.querySelector('.map__pin');
 var pinList = document.querySelector('.map__pins');
 var mainPin = pinList.querySelector('.map__pin--main');
-var cardTemplate = document.querySelector('#card').content.querySelector('.map__card');
 var fieldsets = document.querySelectorAll('fieldset');
 var filtersFields = filtersContainer.querySelectorAll('select');
 var adForm = document.querySelector('.ad-form');
@@ -233,7 +232,7 @@ var makePin = function (pinInfo) {
     if (mapCard) {
       mapCard.remove();
     }
-    var newCard = renderCard(pinInfo);
+    var newCard = window.card.renderCard(pinInfo);
     map.insertBefore(newCard, filtersContainer);
 
     var popupClose = document.querySelector('.popup__close');
@@ -245,71 +244,10 @@ var makePin = function (pinInfo) {
 
   pinElement.addEventListener('keydown', function (evt) {
     if (evt.keyCode === ENTER_KEYCODE) {
-      var newCard = renderCard(pinInfo);
+      var newCard = window.card.renderCard(pinInfo);
       map.insertBefore(newCard, filtersContainer);
     }
   });
 
   return pinElement;
-};
-
-var getTranslation = function (type) {
-  var translation = '';
-
-  if (type === 'flat') {
-    translation = 'Квартира';
-  } else if (type === 'bungalo') {
-    translation = 'Бунгало';
-  } else if (type === 'house') {
-    translation = 'Дом';
-  } else if (type === 'palace') {
-    translation = 'Дворец';
-  }
-
-  return translation;
-};
-
-var getAllFeatures = function (records) {
-  var fragmentLi = document.createDocumentFragment();
-  for (i = 0; i < records.length; i++) {
-    var newLi = document.createElement('li');
-    newLi.classList.add('popup__feature', 'popup__feature--' + records[i]);
-    fragmentLi.appendChild(newLi);
-  }
-  return fragmentLi;
-};
-
-var fillFeatures = function (features, featuresList) {
-  featuresList.innerHTML = '';
-  featuresList.appendChild(features);
-};
-
-var getPhotos = function (photos, photosList) {
-  var firstImg = photosList.querySelector('.popup__photo');
-  firstImg.src = photos[0];
-
-  for (i = 1; i < photos.length; i++) {
-    var newPhoto = firstImg.cloneNode();
-    newPhoto.src = photos[i];
-    photosList.appendChild(newPhoto);
-  }
-};
-
-var renderCard = function (card) {
-  var cardElement = cardTemplate.cloneNode(true);
-  var featuresList = cardElement.querySelector('.popup__features');
-  var photosList = cardElement.querySelector('.popup__photos');
-
-  cardElement.querySelector('.popup__title').textContent = card.offer.title;
-  cardElement.querySelector('.popup__text--address').textContent = card.offer.address;
-  cardElement.querySelector('.popup__text--price').textContent = card.offer.price + '₽/ночь';
-  cardElement.querySelector('.popup__type').textContent = getTranslation(card.offer.type);
-  cardElement.querySelector('.popup__text--capacity').textContent = card.offer.rooms + ' комнаты для ' + card.offer.guests + ' гостей';
-  cardElement.querySelector('.popup__text--time').textContent = 'Заезд после ' + card.offer.checkin + ', выезд до ' + card.offer.checkout;
-  fillFeatures(getAllFeatures(card.offer.features), featuresList);
-  cardElement.querySelector('.popup__description').textContent = card.offer.description;
-  cardElement.querySelector('.popup__avatar').src = card.author.avatar;
-  getPhotos(card.offer.photos, photosList);
-
-  return cardElement;
 };
